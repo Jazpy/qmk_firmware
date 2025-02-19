ab0669f093fe5741ea299a64a12fe5f799cf37d1
Jazpy <jaz.medtre@gmail.com>
2025-02-19 12:35:02 -0500

    Update keymap.c

--- keyboards/salicylic_acid3/7skb/keymaps/default/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/default/keymap.c
@@ -19,11 +19,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_GRV,
+       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS, KC_TILDE,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
@@ -33,29 +33,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN] = LAYOUT(
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-  TG(_ADJUST),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,     _______, _______, KC_PSCR, KC_SCRL,KC_PAUSE,   KC_UP, _______, KC_BSPC,
+      _______, _______, KC_UP, _______, _______, _______,     _______, _______, _______, _______,_______,  KC_PGUP, KC_PGDN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,     _______, _______, KC_HOME, KC_PGUP, KC_LEFT,KC_RIGHT, _______,
+      _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,     _______, _______, _______, _______, _______,_______, _______,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,     _______, _______,  KC_END, KC_PGDN, KC_DOWN, _______, _______,
+      _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               _______, _______, _______, _______,              _______, _______,         KC_STOP, FUCK_OFF
+            TG(_ADJUST), _______, _______, _______,              _______, _______,         _______, FUCK_OFF
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
   [_ADJUST] = LAYOUT( /* Base */
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-  TG(_ADJUST), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   QK_BOOT,
+  TG(_ADJUST), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     UG_TOGG, UG_NEXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     UG_VALD, UG_VALU, UG_HUED, UG_HUEU, UG_SATD, UG_SATU, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX,          KC_STOP, XXXXXXX
+               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX
           //`---------------------------------------------|   |--------------------------------------------'
   )
 };
@@ -65,7 +65,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-void click_buttons(int32_t *x_coords, int32_t *y_coords, size_t arr_len) {
+void click_buttons(int32_t *x_coords, int32_t *y_coords, size_t arr_len, bool click) {
     // TOP LEFT
     int32_t curr_x = 0;
     int32_t curr_y = 0;
@@ -125,38 +125,66 @@ void click_buttons(int32_t *x_coords, int32_t *y_coords, size_t arr_len) {
         wait_ms(100);
 
         // CLICK
-        tap_code(MS_BTN1);
-        wait_ms(100);
+        if (click)
+            tap_code(MS_BTN1);
+        wait_ms(5000);
     }
 }
 
 uint32_t refresh_callback(uint32_t trigger_time, void *cb_arg) {
     // Button positions
-    int32_t x_coords[] = {0};
-    int32_t y_coords[] = {0};
+    int32_t x_coords[] = {460};
+    int32_t y_coords[] = {5};
     size_t arr_len = sizeof(x_coords) / sizeof(x_coords[0]);
 
     // Click'em
-    click_buttons(x_coords, y_coords, arr_len);
+    click_buttons(x_coords, y_coords, arr_len, true);
 
     // Refresh page
     tap_code(KC_F5);
 
-    // Random cooldown between 15 and 25 seconds
-    return 15000 + (rand() % 10000);
+    // Refresh in one minute
+    return 60000;
 }
 
 uint32_t report_callback(uint32_t trigger_time, void *cb_arg) {
     // Button positions
-    int32_t x_coords[] = {300, 0};
-    int32_t y_coords[] = {300, 0};
+    int32_t start_activity_x = 70;
+    int32_t start_activity_y = 110;
+    int32_t activity_x = 250;
+    int32_t activity_y = 110;
+    int32_t start_x = 250;
+    int32_t start_y = 182;
+    
+    int32_t x_coords[] = {start_activity_x, start_activity_x, activity_x};
+    int32_t y_coords[] = {start_activity_y, start_activity_y, activity_y};
     size_t arr_len = sizeof(x_coords) / sizeof(x_coords[0]);
 
     // Click'em
-    click_buttons(x_coords, y_coords, arr_len);
+    click_buttons(x_coords, y_coords, arr_len, true);
 
-    // Random cooldown between 2:30 and 3:10 minutes
-    return 150000 + (rand() % 40000);
+    // 'inv' string
+    tap_code(KC_I);
+    wait_ms(500);
+    tap_code(KC_N);
+    wait_ms(500);
+    tap_code(KC_V);
+    wait_ms(500);
+    tap_code(KC_DOWN);
+    wait_ms(500);
+    tap_code(KC_ENT);
+    wait_ms(500);
+
+    // Final button
+    int32_t x_coords_f[] = {start_x};
+    int32_t y_coords_f[] = {start_y};
+    arr_len = sizeof(x_coords_f) / sizeof(x_coords_f[0]);
+
+    // Click'em
+    click_buttons(x_coords_f, y_coords_f, arr_len, true);
+    
+    // Random cooldown between 3:30 and 3:50 minutes
+    return 210000 + (rand() % 20000);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -167,7 +195,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     if (record->event.pressed && keycode == FUCK_OFF && !fuck_off_mode) {
         token_0 = defer_exec(1, report_callback, NULL);
-        token_1 = defer_exec(1, refresh_callback, NULL);
+        token_1 = defer_exec(600, refresh_callback, NULL);
         fuck_off_mode = true;
     } else if (record->event.pressed && fuck_off_mode) {
         cancel_deferred_exec(token_0);
